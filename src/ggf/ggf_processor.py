6443a89f856d883b3527ef7e04fa03b40fd1640b
dabillox <danielfisher0@gmail.com>
2017-11-18 10:20:55 +0000

    changed col names in flare extraction and removed indexing from csv outputs

--- src/ggf/ggf_processor.py
+++ src/ggf/ggf_processor.py
@@ -186,7 +186,7 @@ def main():
     if proc_const.sensor.upper() not in output_fname:
         output_fname = output_fname.replace(output_fname[0:3], proc_const.sensor.upper())
     csv_path = os.path.join(path_to_output, output_fname)
-    df.to_csv(csv_path)
+    df.to_csv(csv_path, index=False)
 
 if __name__ == "__main__":
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
