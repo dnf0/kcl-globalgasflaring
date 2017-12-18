10e386825ab42ccdff3f1b993050bf5eaf09129c
dabillox <danielfisher0@gmail.com>
2017-12-18 13:06:28 +0000

    updated sample counter to include cloud free and falre counts

--- src/data/sample_detection/l3_merge_orbits_to_time_series.py
+++ src/data/sample_detection/l3_merge_orbits_to_time_series.py
@@ -21,31 +21,33 @@ def main():
     csv_filepaths = [f for f in csv_filepaths if not re.search(r'at1/1995/[1][0-2]/', f)]
 
     sample_counter = defaultdict(int)
+    cloud_free_counts = defaultdict(int)
+    flare_counts = defaultdict(int)
     lats = defaultdict(float)
     lons = defaultdict(float)
 
     # now lets get count the samples
-    max_id = 0
     for f in csv_filepaths:
         try:
             sample_df = pd.read_csv(f)
-            s_max = sample_df.flare_ids.max()
-            if s_max > max_id:
-                print s_max
-                max_id = s_max
-                print f
-                print 
+
             for index, row in sample_df.iterrows():
                 sample_counter[row.flare_ids] += 1
                 lats[row.flare_ids] = row.matched_lats
                 lons[row.flare_ids] = row.matched_lons
+                if row.obs_types == 2:
+                    cloud_free_counts[row.flare_ids] += 1
+                if row.obs_types == 1:
+                    flare_counts[row.flare_ids] += 1
 
         except Exception, e:
             logger.warning('Could not load csv file with error: ' + str(e))
 
-    out_df = pd.DataFrame({"flare_sample_counts": sample_counter,
-                           "flare_sample_lats": lats,
-                           "flare_sample_lons": lons})
+    out_df = pd.DataFrame({"sample_counts": sample_counter,
+                           "cloud_free_counts": cloud_free_counts,
+                           "flare_counts": flare_counts,
+                           "sample_lats": lats,
+                           "sample_lons": lons})
 
     # dump to csv
     if not os.path.exists(os.path.join(fp.path_to_cems_output_l3, 'all_sensors')):
