bbb0643efc243e116d3a3ef9d93af35b15437f74
dabillox <danielfisher0@gmail.com>
2018-01-31 10:58:20 +0000

    added in workaround script to remove temp files that contain .panfs files during sls proc.

---
+++ src/ggf/ggf_sls_file_remover.py
@@ -0,0 +1,25 @@
+'''
+Scans sls temp dir and removes any empty
+files
+'''
+
+import logging
+import os
+
+import src.config.filepaths as fp
+
+
+def main():
+    while True:
+        for f in os.listdir(fp.path_to_temp):
+            try:
+                os.rmdir(f)
+            except:
+                continue
+
+
+if __name__ == "__main__":
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger(__name__)
+    main()
