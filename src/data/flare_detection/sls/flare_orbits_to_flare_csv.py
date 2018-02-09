5d5ebee181fa96bc2ae4478429da5eef17cb4dd5
dabillox <danielfisher0@gmail.com>
2018-02-09 18:35:37 +0000

    now aggregating flares

--- src/data/flare_detection/sls/flare_orbits_to_flare_csv.py
+++ src/data/flare_detection/sls/flare_orbits_to_flare_csv.py
@@ -18,16 +18,17 @@ def main():
     cols = ['frp', 'swir_radiances', 'mwir_radiances', 'mwir_bg',
              'pixel_size', 'lats', 'lons', 'lats_arcmin',
             'lons_arcmin', 'year', 'month', 'day', 'hhmm', 'sensor',
-            'se_dist', 'frp_coeff', 'cloud_cover', 'hotspot_bg_pc', 'inval_pixels_bg_pc', 'bg_size_used']
+            'frp_coeff', 'cloud_cover', 'hotspot_bg_pc', 'inval_pixels_bg_pc', 'bg_size_used',
+            'sza', 'vza']
     dtypes = {'frp': float, 'swir_radiances': float, 'mwir_radiances': float, 'mwir_bg': float,
              'pixel_size': float, 'lats': float, 'lons': float, 'lats_arcmin': int,
             'lons_arcmin': int, 'year': int, 'month': int, 'day': int, 'hhmm': int, 'sensor': str,
             'se_dist': float, 'frp_coeff': float, 'cloud_cover': float, 'hotspot_bg_pc': float,
-            'inval_pixels_bg_pc': float, 'bg_size_used': int}
+            'inval_pixels_bg_pc': float, 'bg_size_used': int, 'sza': float, 'vza': float}
 
     csv_filepaths = glob.glob(fp.path_to_cems_output_l2 + '*/*/*/*/*_flares.csv')
     for f in csv_filepaths:
-        if 'S3A' in f:
+        if not 'S3A' in f:
             continue
         df_list.append(pd.read_csv(f, usecols=cols, dtype=dtypes))
 
