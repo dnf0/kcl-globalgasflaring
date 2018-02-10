08d5b97302a880bda57e95f691f8bb59bcc01c48
dabillox <danielfisher0@gmail.com>
2018-02-10 08:03:35 +0000

    added filenames to differentiate between sls and atx.  removed uneeded sample detection scripts

--- src/data/flare_detection/atx/flare_orbits_to_flare_csv.py
+++ src/data/flare_detection/atx/flare_orbits_to_flare_csv.py
@@ -36,7 +36,7 @@ def main():
     path_to_out = os.path.join(fp.path_to_cems_output_l3, 'all_sensors')
     if not os.path.exists(path_to_out):
         os.makedirs(path_to_out)
-    df.to_csv(os.path.join(path_to_out, 'all_flares.csv'))
+    df.to_csv(os.path.join(path_to_out, 'all_flares_atx.csv'))
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
