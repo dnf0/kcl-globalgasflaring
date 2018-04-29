915495da606b911f40d665d898c5f7cc738495b1
dabillox <danielfisher0@gmail.com>
2018-04-29 09:15:34 +0100

    added in batch processor for l3 processing and changed batch proc for l2 data so that it does not submit jobs for sls files that have already been processed.  This is needed as SSH into cems is often killed before all jobs are submitted.  But we do not want to resbumit all jobs each time.  This stops that.

--- src/data/batch_proc.py
+++ src/data/batch_proc.py
@@ -55,111 +55,17 @@ class BatchSystem:
             return m.groupdict()
 
 
-def check_atx_year(ymd):
-    if 'at2' in f.lower():
-        if int(ymd[0:6]) <= 200306:
-            return True
-    elif 'at1' in f.lower():
-        if int(ymd[0:6]) <= 199605:
-            return True
-    else:
-        return True  # we process all ATS data and this checks for that
+def submit_job():
 
-
-def make_outpath_atx(f, ymd):
-
-    if 'N1' in f:
-        sensor = 'ats'
-    if 'E2' in f:
-        sensor = 'at2'
-    if 'E1' in f:
-        sensor = 'at1'
-
-    out_dir = os.path.join(filepaths.path_to_cems_output_l2, sensor,
-                           ymd[0:4], ymd[4:6], ymd[6:8])
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    return out_dir
-
-
-def submit_atx(root, f):
-
-    if f.split('.')[-1] not in ['N1', 'E2', 'E1']:
-        return
-
-    data_path = os.path.join(root, f)
-
-    # check year, month and sensor to see if we are going to process
-    ymd = f[14:22]
-    if not check_atx_year(ymd):
-        #print 'Did not submit job for file:', f
-        return
-    #else:
-        #print 'Submitting file job for file:', f
-
-    # construct ouptut path
-    out_dir = make_outpath_atx(f, ymd)
+    temp_dir =
 
     # for each ATSR file generate a bash script that calls ggf
     (gd, script_file) = tempfile.mkstemp('.sh', 'ggf.',
-                                         out_dir, True)
+                                         temp_dir, True)
     g = os.fdopen(gd, "w")
     g.write('export PYTHONPATH=$PYTHONPATH:/home/users/dnfisher/projects/kcl-globalgasflaring/\n')
-    g.write(filepaths.ggf_dir + python_exe +
-            data_path + ' ' +
-            out_dir + " \n")
+    g.write(filepaths.ggf_dir + python_exe + " \n")
     g.write("rm -f " + script_file + "\n")
-    g.close()
-    os.chmod(script_file, 0o755)
-
-    # generate bsub call using print_batch
-    cmd = batch.print_batch(batch_values, exe=script_file)
-
-    # use subprocess to call the print batch command
-    try:
-        out = subprocess.check_output(cmd.split(' '))
-        jid = batch.parse_out(out, 'ID')
-    except Exception, e:
-        print 'Subprocess failed with error:', str(e)
-
-
-def make_outpath_sls(f, ymd):
-    sensor = 'sls'
-    out_dir = os.path.join(filepaths.path_to_cems_output_l2, sensor,
-                           ymd[0:4], ymd[4:6], ymd[6:8])
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    return out_dir
-
-
-def submit_sls(root, f):
-
-    if '.zip' not in f:
-        return
-
-    ymd = f[16:24]
-    path_to_data = os.path.join(root, f)
-    out_dir = make_outpath_sls(f, ymd)
-
-    # check if we have already processed the file and skip if so
-    output_fname = f.split('.')[0] + '_hotspots.csv'
-    if os.path.isfile(os.path.join(out_dir, output_fname)):
-        return
-
-    temp_dir = filepaths.path_to_temp
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-
-    (gd, script_file) = tempfile.mkstemp('.sh', 'ggf.',
-                                         out_dir, True)
-    g = os.fdopen(gd, "w")
-    g.write('export PYTHONPATH=$PYTHONPATH:/home/users/dnfisher/projects/kcl-globalgasflaring/\n')
-    g.write(filepaths.ggf_dir + python_exe +
-            path_to_data + ' ' +
-            out_dir + ' ' +
-            temp_dir + " \n")
-    g.write("rm -f " + script_file + "\n")
-
     g.close()
     os.chmod(script_file, 0o755)
 
@@ -192,24 +98,12 @@ batch_values = {'email'    : 'danielfisher0@gmail.com'}
 
 
 # define python script to run
-python_exe = 'ggf_extract_hotspots_sls.py '
+stage = 'flare_detection'
+sensor = 'atx'
+script = 'hotspot_months_to_flare_locations.py '
 
-if 'atx' in python_exe:
-    paths = filepaths.paths_to_atx_data
-else:
-    paths = filepaths.path_to_sls_data
+# submit job
+python_exe = os.path.join(stage, sensor, script)
+submit_job()
 
-# iterate over all ATSR files in directory
-for path_to_data in paths:
-    years = os.listdir(path_to_data)
-    for yr in years:
-        if len(yr) > 4:
-            continue
-        path = os.path.join(path_to_data,  yr)
-        for root, dirs, files in os.walk(path, followlinks=True):
-            for f in files:
-                if 'atx' in python_exe:
-                    submit_atx(root, f)
-                else:
-                    submit_sls(root, f)
 
