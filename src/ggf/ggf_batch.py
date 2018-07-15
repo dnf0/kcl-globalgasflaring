4143db8e1f81dcced76884da20f5a63117d44fd6
dabillox <danielfisher0@gmail.com>
2018-07-15 21:18:08 +0100

    removed at1 only processing flag

--- src/ggf/ggf_batch.py
+++ src/ggf/ggf_batch.py
@@ -219,8 +219,8 @@ for path_to_data in paths:
     for yr in years:
         if len(yr) > 4:
             continue
-        if (int(yr) > 1992):
-            continue
+        #if (int(yr) > 1992):
+        #    continue
         print yr
         path = os.path.join(path_to_data,  yr)
         for root, dirs, files in os.walk(path, followlinks=True):
